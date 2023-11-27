04204b7eab0b1e148db629d757a2f5f40722ae76
TeeNik <teenik3315@gmail.com>
2023-11-27 22:28:21 +0100

    Shapes script with Sphere struct

---
+++ Assets/Scripts/Shapes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Sphere
+{
+    public Vector3 pos;
+    public float radius;
+    public Vector3 albedo;
+    public Vector3 specular;
+
+}
