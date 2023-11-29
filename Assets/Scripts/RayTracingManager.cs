aae24676ef45dd3ebb3247fdb0d289e9b8f6e046
TeeNik <teenik3315@gmail.com>
2023-11-29 23:00:13 +0100

    generate scene of random spheres

--- Assets/Scripts/RayTracingManager.cs
+++ Assets/Scripts/RayTracingManager.cs
@@ -8,6 +8,11 @@ public class RayTracingManager : MonoBehaviour
     public Texture SkyboxTexture;
     public Light DirectionalLight;
 
+    public Vector2 SphereRadius = new Vector2(3.0f, 8.0f);
+    public uint SpheresMax = 100;
+    public float SpherePlacementRadius = 100.0f;
+    private ComputeBuffer _spheresBuffer;
+
     private RenderTexture _target;
     private Camera _camera;
     private int _currentSample = 0;
@@ -16,6 +21,70 @@ public class RayTracingManager : MonoBehaviour
     private void Awake()
     {
         _camera = GetComponent<Camera>();
+    }
+
+    private void OnEnable()
+    {
+        _currentSample = 0;
+        SetUpScene();
+        
+    }
+
+    private void OnDisable()
+    {
+        if (_spheresBuffer != null)
+        {
+            _spheresBuffer.Release();
+        }
+    }
+
+    private void SetUpScene()
+    {
+        List<Sphere> spheres = new List<Sphere>();
+        for (int i = 0; i < SpheresMax; ++i)
+        {
+            Sphere sphere = new Sphere();
+            sphere.radius = Random.Range(SphereRadius.x, SphereRadius.y);
+
+            int tries = 0;
+            while (true)
+            {
+                bool collided = false;
+                Vector2 randomPos = Random.insideUnitCircle * SpherePlacementRadius;
+                sphere.pos = new Vector3(randomPos.x, sphere.radius, randomPos.y);
+
+                foreach (var other in spheres)
+                {
+                    float minDist = sphere.radius + other.radius;
+                    if (Vector3.SqrMagnitude(sphere.pos - other.pos) < minDist * minDist)
+                    {
+                        collided = true;
+                        break;
+                    }
+                }
+
+                ++tries;
+                if (tries >= 1000)
+                {
+                    Debug.LogError("Failed try to place a sphere without colliding with others");
+                    break;
+                }
+
+                if (!collided)
+                {
+                    break;
+                }
+            }
+
+            Color color = Random.ColorHSV();
+            bool metal = Random.value < 0.5f;
+            sphere.albedo = metal ? Vector3.zero : new Vector3(color.r, color.g, color.b);
+            sphere.specular = metal ? new Vector3(color.r, color.g, color.b) : Vector3.one * 0.04f;
+            spheres.Add(sphere);
+        }
+
+        _spheresBuffer = new ComputeBuffer(spheres.Count, 40);
+        _spheresBuffer.SetData(spheres);
     }
 
     private void Update()
@@ -35,6 +104,7 @@ public class RayTracingManager : MonoBehaviour
 
     private void SetShaderParameters()
     {
+        RayTracingShader.SetBuffer(0, "_Spheres", _spheresBuffer);
         RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);
         RayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
         RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
