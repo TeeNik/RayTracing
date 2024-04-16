ddb0be56b6bb2734dc64670c28fc395445e0da64
TeeNik <teenik3315@gmail.com>
2024-04-16 23:26:19 +0200

    simple blur

--- Assets/Scripts/BloomEffect.cs
+++ Assets/Scripts/BloomEffect.cs
@@ -1,19 +1,31 @@
+using System;
 using UnityEngine;
 
 [ExecuteInEditMode, ImageEffectAllowedInSceneView]
 public class BloomEffect : MonoBehaviour
 {
     [Range(1, 16)] public int Iterations = 1;
-
+    public Shader BloomShader;
+    
+    [NonSerialized] private Material _bloom;
     private RenderTexture[] _textures = new RenderTexture[16];
+
+    private const int BoxDownPass = 0;
+    private const int BoxUpPass = 1;
     
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
+        if (_bloom == null)
+        {
+            _bloom = new Material(BloomShader);
+            _bloom.hideFlags = HideFlags.HideAndDontSave;
+        }
+        
         int width = source.width / 2;
         int height = source.height / 2;
         RenderTextureFormat format = source.format;
         RenderTexture currentDest = _textures[0] = RenderTexture.GetTemporary(width, height, 0, format);
-        Graphics.Blit(source, currentDest);
+        Graphics.Blit(source, currentDest, _bloom, BoxDownPass);
 
         RenderTexture currentSource = currentDest;
 
@@ -27,7 +39,7 @@ public class BloomEffect : MonoBehaviour
                 break;
             }
             currentDest = _textures[i] = RenderTexture.GetTemporary(width, height, 0, format);
-            Graphics.Blit(currentSource, currentDest);
+            Graphics.Blit(currentSource, currentDest, _bloom, BoxDownPass);
             currentSource = currentDest;
         }
 
@@ -35,13 +47,13 @@ public class BloomEffect : MonoBehaviour
         {
             currentDest = _textures[i];
             _textures[i] = null;
-            Graphics.Blit(currentSource, currentDest);
+            Graphics.Blit(currentSource, currentDest, _bloom, BoxUpPass);
             RenderTexture.ReleaseTemporary(currentSource);
             currentSource = currentDest;
         }
         
         
-        Graphics.Blit(currentSource, destination);
+        Graphics.Blit(currentSource, destination, _bloom, BoxUpPass);
         RenderTexture.ReleaseTemporary(currentSource);
     }
 }
