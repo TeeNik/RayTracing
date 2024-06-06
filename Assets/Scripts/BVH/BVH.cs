fd0f405b087bc65bd9579539d824881e914d60f6
TeeNik <teenik3315@gmail.com>
2024-06-06 11:40:30 +0200

    construct BHV [1]

--- Assets/Scripts/BVH/BVH.cs
+++ Assets/Scripts/BVH/BVH.cs
@@ -2,17 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Node
+{
+    public BoundingBox Bounds = new();
+    public List<BVHTriangle> Triangles = new();
+    public Node ChildA;
+    public Node ChildB;
+
+    public Node(BoundingBox bounds, List<BVHTriangle> triangles)
+    {
+        Bounds = bounds;
+        Triangles = triangles;
+    }
+    
+}
+
 public class BVH : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public Node root;
+    
+    public BVH(Vector3[] vertices, int[] indices)
     {
-        
+        BoundingBox bounds = new BoundingBox();
+
+        foreach (var vertex in vertices)
+        {
+            bounds.GrowToInclude(vertex);
+        }
+
+        BVHTriangle[] triangles = new BVHTriangle[indices.Length / 3];
+        for (int i = 0; i < indices.Length; ++i)
+        {
+            Vector3 a = vertices[indices[i + 0]];
+            Vector3 b = vertices[indices[i + 1]];
+            Vector3 c = vertices[indices[i + 2]];
+            triangles[i] = new BVHTriangle(a, b, b);
+        }
+
+        root = new Node(bounds, new List<BVHTriangle>(triangles));
     }
 }
--- Assets/Scripts/BVH/BoundingBox.cs
+++ Assets/Scripts/BVH/BoundingBox.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoundingBox : MonoBehaviour
+public class BVHTriangle
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector3 VertexA;
+    public Vector3 VertexB;
+    public Vector3 VertexC;
+
+    public BVHTriangle(Vector3 a, Vector3 b, Vector3 c)
     {
-        
+        VertexA = a;
+        VertexB = b;
+        VertexC = c;
+    }
+}
+
+public class BoundingBox
+{
+    public Vector3 Min = Vector3.positiveInfinity;
+    public Vector3 Max = Vector3.negativeInfinity;
+    public Vector3 Center => (Min + Max) * 0.5f;
+
+    public void GrowToInclude(Vector3 point)
+    {
+        Min = Vector3.Min(Min, point);
+        Max = Vector3.Max(Max, point);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //public void GrowToInclude(BHVTriangle triangle)
+    //{
+    //    GrowToInclude(triangle.VertexA);
+    //    GrowToInclude(triangle.VertexB);
+    //    GrowToInclude(triangle.VertexC);
+    //}
+    
 }
