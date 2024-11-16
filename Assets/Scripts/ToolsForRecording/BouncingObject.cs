d8e38b6fc77df742a572737ff8bdf19c6e3b360e
TeeNik <teenik3315@gmail.com>
2024-11-16 01:17:58 +0100

    add rotation to bouncing objects

--- Assets/Scripts/ToolsForRecording/BouncingObject.cs
+++ Assets/Scripts/ToolsForRecording/BouncingObject.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class BouncingObject : MonoBehaviour, ITickable
 {
     public Vector3 Offset = new Vector3(0, 0.2f, 0);
+    public Vector3 RotationOffset;
     public float Speed = 0.5f;
 
     private float _animationTime = 0.0f;
     private Vector3 _startPos;
+    private Quaternion _startRot;
     
     void Start()
     {
         _startPos = transform.position;
+        _startRot = transform.rotation;
     }
 
     public void Tick(float deltaTime)
     {
         _animationTime += deltaTime;
         transform.position = _startPos + Offset * Mathf.Sin(_animationTime * Speed);
+        transform.rotation = _startRot * Quaternion.Euler(RotationOffset * Mathf.Sin(_animationTime * Speed));
     }
 }
--- Assets/Scripts/ToolsForRecording/TickController.cs
+++ Assets/Scripts/ToolsForRecording/TickController.cs
@@ -1,16 +1,32 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TickController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int FPS = 60;
+    public int FramesBetweemTicks = 10000;
+    public bool TickEveryFrame = false;
+    
+    private List<ITickable> _tickableObjects;
+    private int _ticks = 1;
+    private float _deltaTime = 0.0f;
+    
     void Start()
     {
-        
+        _deltaTime = 1f / FPS;
+        _tickableObjects = FindObjectsByType<MonoBehaviour>(FindObjectsSortMode.None).OfType<ITickable>().ToList();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (TickEveryFrame || Time.frameCount >= FramesBetweemTicks * _ticks)
+        {
+            ++_ticks;
+            foreach (var tickableObject in _tickableObjects)
+            {
+                tickableObject.Tick(_deltaTime);
+            }
+        }
     }
 }
